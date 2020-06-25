21fc6a78e4d2f044c187662f41dcb35b1555d172
hnilsen <hnilsen@HNILSEN-BR.br.oracle.com>
2020-06-24 21:29:34 -0300

    Inicio Projeto

---
+++ ControleAcoes/src/main/java/com/controleacoes/ControleAcoesApplication.java
@@ -0,0 +1,13 @@
+package com.controleacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleAcoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleAcoesApplication.class, args);
+	}
+
+}
---
+++ ControleAcoes/src/test/java/com/controleacoes/ControleAcoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controleacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleAcoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
